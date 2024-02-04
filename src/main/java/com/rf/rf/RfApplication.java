123c7eef1f27b2ec44d62729b6f6774f74035e11
Jaewon Lee <58386334+jaewonLeeKOR@users.noreply.github.com>
2024-02-04 22:15:05 +0900

    init project

---
+++ src/main/java/com/rf/rf/RfApplication.java
@@ -0,0 +1,13 @@
+package com.rf.rf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rf/rf/RfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rf.rf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
